b182151a0763d6a3819f4fd3d985ce583dce6d95
Joseph Smith <jmcsmith@gmail.com>
2018-05-18 11:30:05 -0400

    Initial Commit

---
+++ workout-timers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  workout-timers
+//
+//  Created by Joseph Smith on 5/18/18.
+//  Copyright © 2018 Robotic Snail Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
